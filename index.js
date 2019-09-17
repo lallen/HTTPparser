f96320f3a304e7d61492518318f3260213d9adfb
lallen <lallen@trongo.lallen>
2019-09-17 19:13:58 -0400

    added multiple header support but broken

--- index.js
+++ index.js
@@ -9,13 +9,20 @@ module.exports = function parseHttp(request) {
 	 parsedMessage.verb = parts[0].toUpperCase()
 	 parsedMessage.path = parts[1].toUpperCase()
 	 parsedMessage.version = parts[2].toUpperCase()
+	
+	if (lines[1] === undefined){
 	return parsedMessage
+	}
 
 	parsedMessage.headers = {}
 	parts = lines[1].split(': ')
+
+	for (i = 1; i < lines.length(); i++){ 
 	const key = parts[0]
 	const value = parts[1]
 	parsedMessage.headers[key] = value
+	}
+
 	return parsedMessage
 
 }
--- spec/parserSpec.js
+++ spec/parserSpec.js
@@ -51,4 +51,24 @@ describe('parse http', function() {
 				'content type': 'text/HTML'
 			}}
 	})
+	
+	it ('handles multiple headers', function() {
+
+		//arrange
+		const request = 'GET / HTTP/1.1\ncontent-type: text/HTML; charset = utf-8'
+		
+		//act
+		const output = parseHttp(request)
+
+	        //assert
+		const expected = {
+			verb: 'GET',
+			path: '/',
+			version: 'HTTP/1.1',
+			headers: {
+				'content type': 'text/HTML'
+			//	'Accept': 'text/HTML'
+			}}
+	})
+
 })
